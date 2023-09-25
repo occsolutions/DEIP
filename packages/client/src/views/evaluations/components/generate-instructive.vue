c29bbc5b1307870d348fbaa3badb40f156e7daf0
Ramón Rivas <ramon.rivas@alphas.technology>
2023-09-25 08:48:49 -0400

    fix(client): limpia generación de instructive y condiciona según si es leader o no

--- packages/client/src/views/evaluations/components/generate-instructive.vue
+++ packages/client/src/views/evaluations/components/generate-instructive.vue
@@ -8,13 +8,6 @@
     >
       <v-icon class="mr-3">fa-file-pdf</v-icon> {{ $t('Views.Evaluations.stepEvaluatedSelection.generateInstructive.download_instructive') }}
     </v-btn>
-    <img v-for="i in 8" :key="i"
-      :src="'/img/step_' + i + '_excel.png'"
-      style="visibility:hidden" :id="'excelImage'+i"
-      width="0"
-      height="0"
-      alt="Excel step"
-    />
   </div>
 </template>
 
@@ -27,28 +20,18 @@ import pdffonts from 'pdfmake/build/vfs_fonts.js'
 pdfmake.vfs = pdffonts.pdfMake.vfs
 
 export default Vue.extend({
-  data () {
-    return {
-      // imageStep1: null,
-      // imageStep2: null,
-      // imageStep3: null,
-      // imageStep4: null,
-      // imageStep5: null,
-      // imageStep6: null,
-      // imageStep7: null,
-      // imageStep8: null,
-      // imagesBase64: []
-    }
+  props: {
+    leader: Boolean
   },
   methods: {
     downloadInstructive () {
       this.$store.dispatch('loading/show')
       const docDefinition = {
         content: [
-          { text: this.$t('instructive.title'), fontSize: 20, alignment: 'center', margin: [70, 35, 70, 10] },
+          { text: this.$t(`instructive.title${this.leader ? '_leaders' : ''}`), fontSize: 20, alignment: 'center', margin: [70, 35, 70, 10] },
           { text: this.$t('instructive.title_body'), fontSize: 12, margin: [40, 10, 30, 10] },
           { text: this.$t('instructive.use_methods'), fontSize: 17, bold: true, alignment: 'left', margin: [40, 20, 30, 10] },
-          { text: this.$t('instructive.description'), style: 'paragraph' },
+          { text: this.$t(`instructive.description${this.leader ? '_leaders' : ''}`), style: 'paragraph' },
           { text: this.$t('instructive.example'), style: 'h2' },
           {
             layout: 'lightHorizontalLines', // optional
@@ -56,20 +39,12 @@ export default Vue.extend({
               widths: [300, 150],
               body: [
                 ['email'],
-                ['participante1@email.com'],
-                ['participante2@email.com'],
-                ['participante3@email.com'],
-                ['participante4@email.com'],
-                ['participante5@email.com'],
-                ['participante6@email.com'],
-                ['participante7@email.com'],
-                ['participante8@email.com'],
-                ['participante9@email.com'],
-                ['participante10@email.com'],
-                ['participante11@email.com'],
-                ['participante12@email.com'],
-                ['participante13@email.com'],
-                ['participante14@email.com']
+                ...(([`participante${this.leader ? '_leader' : ''}@email.com`].map(email => {
+                  const parts = email.split('@')
+                  const emails = []
+                  for (let i = 1; i <= 17; i++) emails.push([`${parts[0]}${i}@${parts[1]}`])
+                  return emails
+                }))[0])
               ]
             },
             margin: [40, 5, 30, 10],
@@ -77,20 +52,6 @@ export default Vue.extend({
           }
         ]
       }
-
-      /*
-      docDefinition.content.push({
-        text: this.$t('instructive.importDataToExcelTitle'),
-        style: 'h2'
-      })
-      for (let i = 1; i < 9; i++) {
-        docDefinition.content.push(
-          { text: i !== 8 ? this.$t('instructive.step' + i) : '', style: 'stepper' },
-          { text: this.$t('instructive.step' + i + '_desc'), style: 'paragraph', alignment: 'justify' },
-          { image: this.imagesBase64[i], width: 450, height: 250, margin: [40, 10, 30, 10] }
-        )
-      }
-      */
       docDefinition.styles = {
         paragraph: {
           fontSize: 12,
@@ -131,66 +92,6 @@ export default Vue.extend({
         }).then(() => this.$store.dispatch('loading/hide'))
       }
     }
-    /* end to download help */
-    // toDataURL (url, callback) {
-    //   const xhr = new XMLHttpRequest()
-    //   xhr.open('get', url)
-    //   xhr.responseType = 'blob'
-
-    //   xhr.onload = function () {
-    //     const fr = new FileReader()
-
-    //     fr.onload = function () {
-    //       callback(this.result)
-    //     }
-
-    //     fr.readAsDataURL(xhr.response)
-    //   }
-    //   xhr.send()
-    // },
-    // setDataToUrl (value, idx) {
-    //   if (value) {
-    //     this.toDataURL(value, (dataURL) => {
-    //       this.imagesBase64[idx] = dataURL
-    //     })
-    //   }
-    // }
-  },
-  mounted () {
-    // this.imageStep1 = document.getElementById('excelImage1').src
-    // this.imageStep2 = document.getElementById('excelImage2').src
-    // this.imageStep3 = document.getElementById('excelImage3').src
-    // this.imageStep4 = document.getElementById('excelImage4').src
-    // this.imageStep5 = document.getElementById('excelImage5').src
-    // this.imageStep6 = document.getElementById('excelImage6').src
-    // this.imageStep7 = document.getElementById('excelImage7').src
-    // this.imageStep8 = document.getElementById('excelImage8').src
-  },
-  watch: {
-    // imageStep1 (newVal) {
-    //   this.setDataToUrl(newVal, 1)
-    // },
-    // imageStep2 (newVal) {
-    //   this.setDataToUrl(newVal, 2)
-    // },
-    // imageStep3 (newVal) {
-    //   this.setDataToUrl(newVal, 3)
-    // },
-    // imageStep4 (newVal) {
-    //   this.setDataToUrl(newVal, 4)
-    // },
-    // imageStep5 (newVal) {
-    //   this.setDataToUrl(newVal, 5)
-    // },
-    // imageStep6 (newVal) {
-    //   this.setDataToUrl(newVal, 6)
-    // },
-    // imageStep7 (newVal) {
-    //   this.setDataToUrl(newVal, 7)
-    // },
-    // imageStep8 (newVal) {
-    //   this.setDataToUrl(newVal, 8)
-    // }
   }
 })
 </script>
