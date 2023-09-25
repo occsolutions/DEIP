2f67f411d4cf706ea9403610206dbf44cb093e22
Ramón Rivas <ramon.rivas@alphas.technology>
2023-09-25 08:50:59 -0400

    fix(client): transforma cuestionario de edición de preguntas al nuevo modelo

--- packages/client/src/views/questionnaires/edit.vue
+++ packages/client/src/views/questionnaires/edit.vue
@@ -7,61 +7,71 @@
         </v-col>
       </v-row>
       <v-card>
-        <v-card-text>
-          <v-expansion-panels>
-            <v-expansion-panel
-              v-for="(dimention, ix) in evaluations" :key="ix">
-              <!-- Dimension -->
-              <v-expansion-panel-header class="text-uppercase font-weight-bold">
-                {{ $t(`Views.Questionnaires.edit.d_${ dimention }`) }}
-              </v-expansion-panel-header>
-              <v-expansion-panel-content>
-                <v-row v-for="(variable, i) in Object.keys(questionnaire.evaluations[dimention])" :key="i">
-                  <!-- Variable -->
-                  <v-col cols="12" xs="12">
-                    <v-banner
-                      color="grey lighten-3"
-                      icon="mdi-note-outline"
-                      outlined
-                      rounded
-                      single-line
-                      sticky
-                    >{{ $t(`Views.Questionnaires.edit.v_${ variable }`) }}</v-banner>
-                  </v-col>
-                  <!-- Questions with Reference -->
-                  <v-col cols="12" v-for="(question, q) in questionnaire.evaluations[dimention][variable]" :key="index + '-' + q">
-                    <v-tooltip bottom color="blue" >
-                      <template v-slot:activator="{ on }" >
-                        <v-text-field
-                          light outlined
-                          :label="(question.reference || {})[lang]"
-                          name="question"
-                          v-model="(question.question || {})[lang]"
-                          @keypress.enter="sendForm"
-                          @blur="sendForm"
-                          v-on="on"
-                          outline
-                          append-icon="mdi-content-save-edit-outline"
-                          color="blue"
-                        ></v-text-field>
-                      </template>
-                      <span>
-                        {{ $t('Views.Questionnaires.edit.tooltip_info') }}
-                      </span>
-                    </v-tooltip>
-                  </v-col>
-                </v-row>
-              </v-expansion-panel-content>
-            </v-expansion-panel>
-          </v-expansion-panels>
-          <v-row>
-            <v-col align="end">
-              <v-btn to="/questionnaires" text>
-                {{ $t('Views.Questionnaires.edit.btn_back') }}
-              </v-btn>
-            </v-col>
-          </v-row>
-        </v-card-text>
+        <v-tabs
+          v-model="tab"
+          background-color="transparent"
+          light grow
+        >
+          <v-tab>{{ $t('Views.Questionnaires.edit.dimentions') }}</v-tab>
+          <v-tab>{{ $t('Views.Questionnaires.edit.leader') }}</v-tab>
+        </v-tabs>
+        <v-tabs-items v-model="tab">
+          <v-tab-item>
+            <v-card-text>
+              <v-expansion-panels>
+                <v-expansion-panel
+                  v-for="(dimention, ix) in evaluations" :key="ix">
+                  <!-- Dimension -->
+                  <v-expansion-panel-header class="text-uppercase font-weight-bold">
+                    {{ getLabel(dimention.label) }}
+                  </v-expansion-panel-header>
+                  <v-expansion-panel-content>
+                    <v-row v-for="(attr, i) in dimention.attrs" :key="i">
+                      <!-- Atributo -->
+                      <v-col cols="12" xs="12">
+                        <v-banner
+                          color="grey lighten-3"
+                          icon="mdi-note-outline"
+                          outlined
+                          rounded
+                          single-line
+                          sticky
+                        >{{ getLabel(attr.label) }}</v-banner>
+                      </v-col>
+                      <!-- Questions with Reference -->
+                      <x-input-question
+                        v-for="(question, q) in attr.questions" :key="q"
+                        :question="question"
+                        :slug="questionnaire.slug"
+                        :question-types="types"
+                        :question-key="q"
+                        :lang="lang"
+                      />
+                    </v-row>
+                  </v-expansion-panel-content>
+                </v-expansion-panel>
+              </v-expansion-panels>
+            </v-card-text>
+          </v-tab-item>
+          <v-tab-item>
+            <!-- Questions with Reference -->
+            <x-input-question
+              v-for="(question, q) in leader" :key="q"
+              :question="question"
+              :slug="questionnaire.slug"
+              :question-types="types"
+              :question-key="q"
+              :lang="lang"
+            />
+          </v-tab-item>
+        </v-tabs-items>
+        <v-row class="m-t-4">
+          <v-col align="end">
+            <v-btn to="/questionnaires" text>
+              {{ $t('Views.Questionnaires.edit.btn_back') }}
+            </v-btn>
+          </v-col>
+        </v-row>
       </v-card>
     </template>
   </v-container>
@@ -73,12 +83,19 @@ import Vue from 'vue'
 import { mapState } from 'vuex'
 
 import questionnairesService from '../../services/questionnaires'
+import XInputQuestion from './components/input-question.vue'
 
 export default Vue.extend({
+  components: {
+    XInputQuestion
+  },
   data () {
     return {
+      tab: null,
+      types: [],
       questionnaire: {},
-      evaluations: [],
+      evaluations: {},
+      leader: {},
       lang: 'es'
     }
   },
@@ -91,16 +108,25 @@ export default Vue.extend({
     this.getQuestionnaire()
   },
   methods: {
+    getLabel (label) {
+      return label && typeof label === 'object' ? label[this.lang] || '' : ''
+    },
     getQuestionnaire () {
       this.$store.dispatch('loading/show')
-      return questionnairesService.getOne(this.$route.params.slug)
+      return questionnairesService.questionsTypes()
+        .then(resp => {
+          this.types = resp.items
+          return questionnairesService.getOne(this.$route.params.slug)
+        })
         .then((res) => {
           this.questionnaire = res
           this.$store.dispatch('loading/hide')
           if (!this.questionnaire) {
             this.$router.push('404')
           }
-          this.evaluations = Object.keys(this.questionnaire.evaluations)
+          this.evaluations = JSON.parse(JSON.stringify(this.questionnaire.evaluations))
+          delete this.evaluations.leader
+          this.leader = JSON.parse(JSON.stringify(this.questionnaire.evaluations.leader))
         }).catch(() => {
           this.$store.dispatch('loading/hide')
           this.$router.push('404')
