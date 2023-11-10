f4cb2c0f3dc607d84e56797c8a46a2416b866974
Octavio Torres <octa022@gmail.com>
2023-11-10 13:39:48 -0400

    feat(client): limpia mensaje de agradecimiento al completar encuesta.

--- packages/client/src/views/evaluations/components/end-dialog.vue
+++ packages/client/src/views/evaluations/components/end-dialog.vue
@@ -24,51 +24,6 @@
           >
             {{ $t('Views.Evaluations.evaluation.evaluation_completed') }}
           </h2>
-          <p class="mt-10">
-            {{ $t('Views.Evaluations.evaluation.endDialog.text_1') }}
-          </p>
-          <v-btn
-            color="primary"
-            class="py-5"
-            @click="goToIndividualReport()"
-          >
-            {{ $t('Views.Evaluations.evaluation.endDialog.btn_report') }}
-          </v-btn>
-
-          <p class="mt-10 mb-0">
-            {{
-              alreadySent || sent
-                ? $t('Views.Evaluations.evaluation.endDialog.text_3')
-                : $t('Views.Evaluations.evaluation.endDialog.text_2')
-            }}
-          </p>
-          <ValidationObserver>
-            <ValidationProvider rules="required|email" v-slot="{valid}">
-              <v-text-field
-                v-model="email"
-                :placeholder="$t('help.employee.create.email.title')"
-                color="primary"
-                class="mx-auto"
-                style="max-width: 370px;"
-              >
-                <template v-slot:append>
-                  <v-btn tile
-                    color="primary"
-                    class="ma-0"
-                    :disabled="!valid"
-                    @click="sendEmail()"
-                  >
-                    {{
-                      alreadySent || sent
-                        ? $t('Views.Evaluations.evaluation.endDialog.btn_resend')
-                        : $t('Views.Evaluations.evaluation.endDialog.btn_send')
-                    }}
-                  </v-btn>
-                </template>
-              </v-text-field>
-            </ValidationProvider>
-          </ValidationObserver>
-          <br>
         </div>
         <v-flex>
           <!-- Void -->
@@ -80,59 +35,25 @@
 
 <script>
 import Vue from 'vue'
-import evaluationsService from '../../../services/evaluations'
 
 export default Vue.extend({
   props: {
     lang: String,
-    evaluatedEmployee: Object,
-    sentEmail: String,
     showDialog: Boolean
   },
   data () {
     return {
-      email: '',
-      individualReportRoute: this.$router.resolve({
-        name: 'individualResults'
-      }),
-      sent: false
+      //
     }
   },
   computed: {
-    alreadySent () {
-      return this.sentEmail !== ''
-    }
+    //
   },
   watch: {
-    alreadySent (val) {
-      this.email = val ? this.sentEmail : ''
-    }
+    //
   },
   methods: {
-    goToIndividualReport () {
-      window.open(this.individualReportRoute.href, '_blank')
-    },
-    sendEmail () {
-      this.$store.dispatch('loading/show')
-      const evaluatedName = `${this.evaluatedEmployee.employeeEnterprise.firstName} ${this.evaluatedEmployee.employeeEnterprise.lastName}`
-      evaluationsService.sendIndividualResultsEmail(this.$route.params.tokenId, {
-        lang: this.lang,
-        email: this.email,
-        subject: this.$t('Views.Evaluations.evaluation.endDialog.email_subject'),
-        name: evaluatedName,
-        body: this.$t('Views.Evaluations.evaluation.endDialog.email_body'),
-        url: this.individualReportRoute.href
-      })
-        .then(() => {
-          this.$store.dispatch('loading/hide')
-          this.sent = true
-          this.$store.dispatch('alert/success', this.$t('Views.Evaluations.evaluation.endDialog.email_sent'))
-        })
-        .catch((err) => {
-          this.$store.dispatch('loading/hide')
-          this.$store.dispatch('alert/error', this.$t(`errors.${err.code}`))
-        })
-    }
+    //
   }
 })
 </script>
