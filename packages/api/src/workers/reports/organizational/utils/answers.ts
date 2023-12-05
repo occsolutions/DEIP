c6aedff177adf812633035ec7d92df19f63ad427
Octavio Torres <octa022@gmail.com>
2023-12-04 21:23:33 -0400

    feat(api): adapta utilitario de procesamiento de respuestas, a la estructura final del cuestionario.

--- packages/api/src/workers/reports/organizational/utils/answers.ts
+++ packages/api/src/workers/reports/organizational/utils/answers.ts
@@ -2,73 +2,491 @@
 import { IDimensionScatter } from '../contracts/scatter-dimension';
 import { IAnswersDimension } from '../contracts/answers-dimension';
 
-const dimensionNames = ['physical', 'mental', 'emotional', 'professional'];
-
 class AnswersUtils {
 
   // Answers for Scatter Initializer
-  public iniAnswersForScatter (): IDimensionScatter {
-    const getInitScatter = () => ({ average: 0, scores: [] });
-    const getInitVariable = () => ({
-      questions: {
-        question_1: {
-          scatter: getInitScatter()
-        },
-        question_2: {
-          scatter: getInitScatter()
-        },
-        question_3: {
-          scatter: getInitScatter()
-        },
-        question_4: {
-          scatter: getInitScatter()
-        }
-      }
-    });
-    const getInitDimension = () => ({
-      variables: {
-        var_1: getInitVariable(),
-        var_2: getInitVariable(),
-        var_3: getInitVariable()
-      }
+  public iniAnswersForScatter (isFinal = false): IDimensionScatter {
+    const getInitScatter = () => ({ scatter: isFinal ? 0 : { average: 0, scores: [] } });
+
+    const getInitLeaderScatter = () => ({
+      leader_q1: getInitScatter(),
+      leader_q2: getInitScatter(),
+      leader_q3: getInitScatter(),
+      leader_q4: getInitScatter(),
+      leader_q5: getInitScatter(),
+      leader_q6: getInitScatter(),
+      leader_q7: getInitScatter(),
+      leader_q8: getInitScatter(),
+      leader_q9: getInitScatter(),
+      leader_q10: getInitScatter(),
+      leader_q11: getInitScatter(),
+      leader_q12: getInitScatter()
     });
 
     return {
-      physical: getInitDimension(),
-      mental: getInitDimension(),
-      emotional: getInitDimension(),
-      professional: getInitDimension()
+      d1: {
+        attrs: {
+          d1_attr_1: {
+            questions: {
+              d1_attr_1_q1: getInitScatter(),
+              d1_attr_1_q2: getInitScatter(),
+              d1_attr_1_q3: getInitScatter(),
+            }
+          },
+          d1_attr_2: {
+            questions: {
+              d1_attr_2_q1: getInitScatter(),
+              d1_attr_2_q2: getInitScatter(),
+            }
+          },
+          d1_attr_3: {
+            questions: {
+              d1_attr_3_q1: getInitScatter(),
+            }
+          },
+          d1_attr_4: {
+            questions: {
+              d1_attr_4_q1: getInitScatter(),
+            }
+          },
+          d1_attr_5: {
+            questions: {
+              d1_attr_5_q1: getInitScatter(),
+              d1_attr_5_q2: getInitScatter(),
+              d1_attr_5_q3: getInitScatter(),
+            }
+          },
+          d1_attr_6: {
+            questions: {
+              d1_attr_6_q1: getInitScatter(),
+              d1_attr_6_q2: getInitScatter(),
+            }
+          }
+        }
+      },
+      d2: {
+        attrs: {
+          d2_attr_1: {
+            questions: {
+              d2_attr_1_q1: getInitScatter(),
+            }
+          },
+          d2_attr_2: {
+            questions: {
+              d2_attr_2_q1: getInitScatter(),
+            }
+          },
+          d2_attr_3: {
+            questions: {
+              d2_attr_3_q1: getInitScatter(),
+              d2_attr_3_q2: getInitScatter(),
+              d2_attr_3_q3: getInitScatter(),
+            }
+          },
+          d2_attr_4: {
+            questions: {
+              d2_attr_4_q1: getInitScatter(),
+              d2_attr_4_q2: getInitScatter(),
+            }
+          },
+          d2_attr_5: {
+            questions: {
+              d2_attr_5_q1: getInitScatter(),
+            }
+          },
+          d2_attr_6: {
+            questions: {
+              d2_attr_6_q1: getInitScatter(),
+              d2_attr_6_q2: getInitScatter(),
+              d2_attr_6_q3: getInitScatter(),
+            }
+          },
+          d2_attr_7: {
+            questions: {
+              d2_attr_7_q1: getInitScatter(),
+            }
+          }
+        }
+      },
+      d3: {
+        attrs: {
+          d3_attr_1: {
+            questions: {
+              d3_attr_1_q1: getInitScatter(),
+            }
+          },
+          d3_attr_2: {
+            questions: {
+              d3_attr_2_q1: getInitScatter(),
+              d3_attr_2_q2: getInitScatter(),
+              d3_attr_2_q3: getInitScatter(),
+            }
+          },
+          d3_attr_3: {
+            questions: {
+              d3_attr_3_q1: getInitScatter(),
+              d3_attr_3_q2: getInitScatter(),
+              d3_attr_3_q3: getInitScatter(),
+            }
+          },
+          d3_attr_4: {
+            questions: {
+              d3_attr_4_q1: getInitScatter(),
+              d3_attr_4_q2: getInitScatter(),
+              d3_attr_4_q3: getInitScatter(),
+            }
+          },
+          d3_attr_5: {
+            questions: {
+              d3_attr_5_q1: getInitScatter(),
+            }
+          },
+          d3_attr_6: {
+            questions: {
+              d3_attr_6_q1: getInitScatter(),
+            }
+          }
+        }
+      },
+      d4: {
+        attrs: {
+          d4_attr_1: {
+            questions: {
+              d4_attr_1_q1: getInitScatter(),
+              d4_attr_1_q2: getInitScatter(),
+            }
+          },
+          d4_attr_2: {
+            questions: {
+              d4_attr_2_q1: getInitScatter(),
+            }
+          },
+          d4_attr_3: {
+            questions: {
+              d4_attr_3_q1: getInitScatter(),
+            }
+          },
+          d4_attr_4: {
+            questions: {
+              d4_attr_4_q1: getInitScatter(),
+              d4_attr_4_q2: getInitScatter(),
+            }
+          },
+          d4_attr_5: {
+            questions: {
+              d4_attr_5_q1: getInitScatter(),
+            }
+          },
+          d4_attr_6: {
+            questions: {
+              d4_attr_6_q1: getInitScatter(),
+            }
+          },
+          d4_attr_7: {
+            questions: {
+              d4_attr_7_q1: getInitScatter(),
+              d4_attr_7_q2: getInitScatter(),
+            }
+          }
+        }
+      },
+      d5: {
+        attrs: {
+          d5_attr_1: {
+            questions: {
+              d5_attr_1_q1: getInitScatter(),
+            }
+          },
+          d5_attr_2: {
+            questions: {
+              d5_attr_2_q1: getInitScatter(),
+              d5_attr_2_q2: getInitScatter(),
+            }
+          },
+          d5_attr_3: {
+            questions: {
+              d5_attr_3_q1: getInitScatter(),
+              d5_attr_3_q2: getInitScatter(),
+              d5_attr_3_q3: getInitScatter(),
+            }
+          },
+          d5_attr_4: {
+            questions: {
+              d5_attr_4_q1: getInitScatter(),
+              d5_attr_4_q2: getInitScatter(),
+            }
+          },
+          d5_attr_5: {
+            questions: {
+              d5_attr_5_q1: getInitScatter(),
+              d5_attr_5_q2: getInitScatter(),
+            }
+          }
+        }
+      },
+      leader: getInitLeaderScatter()
     };
   }
 
   // Questionnaire Answers Initializer
   public iniAnswersDimension (): IAnswersDimension {
     const getInitScore = () => ({ score: 0, previous: 0 });
-    const getInitQuestion = () => ({ name: '', general: getInitScore() });
-    const getInitVariable = () => ({
-      name: '',
+    const getInitScores = () => ({ score: 0, scores: [], previous: 0 });
+    const getInitQuestion = () => ({ qType: '', general: getInitScores() });
+    const getInitLeaderDimension = () => ({
       general: getInitScore(),
-      questions: {
-        question_1: getInitQuestion(),
-        question_2: getInitQuestion(),
-        question_3: getInitQuestion(),
-        question_4: getInitQuestion()
-      }
-    });
-    const getInitDimension = () => ({
-      general: getInitScore(),
-      variables: {
-        var_1: getInitVariable(),
-        var_2: getInitVariable(),
-        var_3: getInitVariable()
-      }
+      leader_q1: getInitQuestion(),
+      leader_q2: getInitQuestion(),
+      leader_q3: getInitQuestion(),
+      leader_q4: getInitQuestion(),
+      leader_q5: getInitQuestion(),
+      leader_q6: getInitQuestion(),
+      leader_q7: getInitQuestion(),
+      leader_q8: getInitQuestion(),
+      leader_q9: getInitQuestion(),
+      leader_q10: getInitQuestion(),
+      leader_q11: getInitQuestion(),
+      leader_q12: getInitQuestion()
     });
 
     return {
-      physical: getInitDimension(),
-      mental: getInitDimension(),
-      emotional: getInitDimension(),
-      professional: getInitDimension()
+      d1: {
+        general: getInitScore(),
+        attrs: {
+          d1_attr_1: {
+            general: getInitScore(),
+            questions: {
+              d1_attr_1_q1: getInitQuestion(),
+              d1_attr_1_q2: getInitQuestion(),
+              d1_attr_1_q3: getInitQuestion(),
+            }
+          },
+          d1_attr_2: {
+            general: getInitScore(),
+            questions: {
+              d1_attr_2_q1: getInitQuestion(),
+              d1_attr_2_q2: getInitQuestion(),
+            }
+          },
+          d1_attr_3: {
+            general: getInitScore(),
+            questions: {
+              d1_attr_3_q1: getInitQuestion(),
+            }
+          },
+          d1_attr_4: {
+            general: getInitScore(),
+            questions: {
+              d1_attr_4_q1: getInitQuestion(),
+            }
+          },
+          d1_attr_5: {
+            general: getInitScore(),
+            questions: {
+              d1_attr_5_q1: getInitQuestion(),
+              d1_attr_5_q2: getInitQuestion(),
+              d1_attr_5_q3: getInitQuestion(),
+            }
+          },
+          d1_attr_6: {
+            general: getInitScore(),
+            questions: {
+              d1_attr_6_q1: getInitQuestion(),
+              d1_attr_6_q2: getInitQuestion(),
+            }
+          }
+        }
+      },
+      d2: {
+        general: getInitScore(),
+        attrs: {
+          d2_attr_1: {
+            general: getInitScore(),
+            questions: {
+              d2_attr_1_q1: getInitQuestion(),
+            }
+          },
+          d2_attr_2: {
+            general: getInitScore(),
+            questions: {
+              d2_attr_2_q1: getInitQuestion(),
+            }
+          },
+          d2_attr_3: {
+            general: getInitScore(),
+            questions: {
+              d2_attr_3_q1: getInitQuestion(),
+              d2_attr_3_q2: getInitQuestion(),
+              d2_attr_3_q3: getInitQuestion(),
+            }
+          },
+          d2_attr_4: {
+            general: getInitScore(),
+            questions: {
+              d2_attr_4_q1: getInitQuestion(),
+              d2_attr_4_q2: getInitQuestion(),
+            }
+          },
+          d2_attr_5: {
+            general: getInitScore(),
+            questions: {
+              d2_attr_5_q1: getInitQuestion(),
+            }
+          },
+          d2_attr_6: {
+            general: getInitScore(),
+            questions: {
+              d2_attr_6_q1: getInitQuestion(),
+              d2_attr_6_q2: getInitQuestion(),
+              d2_attr_6_q3: getInitQuestion(),
+            }
+          },
+          d2_attr_7: {
+            general: getInitScore(),
+            questions: {
+              d2_attr_7_q1: getInitQuestion(),
+            }
+          }
+        }
+      },
+      d3: {
+        general: getInitScore(),
+        attrs: {
+          d3_attr_1: {
+            general: getInitScore(),
+            questions: {
+              d3_attr_1_q1: getInitQuestion(),
+            }
+          },
+          d3_attr_2: {
+            general: getInitScore(),
+            questions: {
+              d3_attr_2_q1: getInitQuestion(),
+              d3_attr_2_q2: getInitQuestion(),
+              d3_attr_2_q3: getInitQuestion(),
+            }
+          },
+          d3_attr_3: {
+            general: getInitScore(),
+            questions: {
+              d3_attr_3_q1: getInitQuestion(),
+              d3_attr_3_q2: getInitQuestion(),
+              d3_attr_3_q3: getInitQuestion(),
+            }
+          },
+          d3_attr_4: {
+            general: getInitScore(),
+            questions: {
+              d3_attr_4_q1: getInitQuestion(),
+              d3_attr_4_q2: getInitQuestion(),
+              d3_attr_4_q3: getInitQuestion(),
+            }
+          },
+          d3_attr_5: {
+            general: getInitScore(),
+            questions: {
+              d3_attr_5_q1: getInitQuestion(),
+            }
+          },
+          d3_attr_6: {
+            general: getInitScore(),
+            questions: {
+              d3_attr_6_q1: getInitQuestion(),
+            }
+          }
+        }
+      },
+      d4: {
+        general: getInitScore(),
+        attrs: {
+          d4_attr_1: {
+            general: getInitScore(),
+            questions: {
+              d4_attr_1_q1: getInitQuestion(),
+              d4_attr_1_q2: getInitQuestion(),
+            }
+          },
+          d4_attr_2: {
+            general: getInitScore(),
+            questions: {
+              d4_attr_2_q1: getInitQuestion(),
+            }
+          },
+          d4_attr_3: {
+            general: getInitScore(),
+            questions: {
+              d4_attr_3_q1: getInitQuestion(),
+            }
+          },
+          d4_attr_4: {
+            general: getInitScore(),
+            questions: {
+              d4_attr_4_q1: getInitQuestion(),
+              d4_attr_4_q2: getInitQuestion(),
+            }
+          },
+          d4_attr_5: {
+            general: getInitScore(),
+            questions: {
+              d4_attr_5_q1: getInitQuestion(),
+            }
+          },
+          d4_attr_6: {
+            general: getInitScore(),
+            questions: {
+              d4_attr_6_q1: getInitQuestion(),
+            }
+          },
+          d4_attr_7: {
+            general: getInitScore(),
+            questions: {
+              d4_attr_7_q1: getInitQuestion(),
+              d4_attr_7_q2: getInitQuestion(),
+            }
+          }
+        }
+      },
+      d5: {
+        general: getInitScore(),
+        attrs: {
+          d5_attr_1: {
+            general: getInitScore(),
+            questions: {
+              d5_attr_1_q1: getInitQuestion(),
+            }
+          },
+          d5_attr_2: {
+            general: getInitScore(),
+            questions: {
+              d5_attr_2_q1: getInitQuestion(),
+              d5_attr_2_q2: getInitQuestion(),
+            }
+          },
+          d5_attr_3: {
+            general: getInitScore(),
+            questions: {
+              d5_attr_3_q1: getInitQuestion(),
+              d5_attr_3_q2: getInitQuestion(),
+              d5_attr_3_q3: getInitQuestion(),
+            }
+          },
+          d5_attr_4: {
+            general: getInitScore(),
+            questions: {
+              d5_attr_4_q1: getInitQuestion(),
+              d5_attr_4_q2: getInitQuestion(),
+            }
+          },
+          d5_attr_5: {
+            general: getInitScore(),
+            questions: {
+              d5_attr_5_q1: getInitQuestion(),
+              d5_attr_5_q2: getInitQuestion(),
+            }
+          }
+        }
+      },
+      leader: getInitLeaderDimension()
     };
   }
 
@@ -76,39 +494,62 @@ class AnswersUtils {
     answers: Array<any>,
     answersForScatter: IDimensionScatter,
     answersDimension: IAnswersDimension,
+    isLeader: boolean,
     previous: boolean
   ) {
     const dynamicKey = previous ? 'previous' : 'score';
 
-    // EVALUATIONS
     // Dimensions
-    for (let d = 0; d < 4; d++) {
-      const dimKey = dimensionNames[d];
-      const dimScore = answers[d].score;
-      answersDimension[dimKey].general[dynamicKey] += dimScore;
-
-      // Variables
-      for (let v = 0; v < 3; v++) {
-        const varScore = answers[d].variables[v].score;
-        answersDimension[dimKey].variables[`var_${v + 1}`].name = answers[d].variables[v].name;
-        answersDimension[dimKey].variables[`var_${v + 1}`].general[dynamicKey] += varScore;
-
+    let dimCnt = 0;
+    for (const dimKey of Object.keys(answersDimension)) {
+      if (dimKey !== 'leader') {
+        // Attributes
+        const dimAttributes = answersDimension[dimKey].attrs;
+        for (const attrKey of Object.keys(dimAttributes)) {
+          // Questions
+          const attrQuestions = answersDimension[dimKey].attrs[attrKey].questions;
+          let qCnt = 0;
+          for (const qKey of Object.keys(attrQuestions)) {
+            const question = answers[dimCnt].attribute[qCnt];
+            answersDimension[dimKey].attrs[attrKey].questions[qKey].qType = question.qType;
+            if (['closed', 'likert'].includes(question.qType)) {
+              answersDimension[dimKey].attrs[attrKey].questions[qKey].general[dynamicKey] += question.score[0];
+              if (!previous) {
+                answersForScatter[dimKey].attrs[attrKey].questions[qKey].scatter.scores.push(question.score[0]);
+              }
+            } else {
+              // Options questions
+              answersDimension[dimKey].attrs[attrKey].questions[qKey].general.scores.push(...question.score);
+            }
+            qCnt++;
+          }
+        }
+      } else if (isLeader) {
         // Questions
-        for (let q = 0; q < 4; q++) {
-          const qName = answers[d].variables[v].questions[q].name;
-          const qScore = answers[d].variables[v].questions[q].score;
-          answersDimension[dimKey].variables[`var_${v + 1}`].questions[`question_${q + 1}`].name = qName;
-          answersDimension[dimKey].variables[`var_${v + 1}`].questions[`question_${q + 1}`].general[dynamicKey] += qScore;
-          if (!previous) {
-            answersForScatter[dimKey].variables[`var_${v + 1}`].questions[`question_${q + 1}`].scatter.scores.push(qScore);
+        let qCnt = 0;
+        for (const qKey of Object.keys(answersDimension[dimKey])) {
+          if (!['general', 'filtered'].includes(qKey)) {
+            const question = answers[dimCnt].attribute[qCnt];
+            answersDimension[dimKey][qKey].qType = question.qType;
+            if (['closed', 'likert'].includes(question.qType)) {
+              answersDimension[dimKey][qKey].general[dynamicKey] += question.score[0];
+              if (!previous) {
+                answersForScatter[dimKey][qKey].scatter.scores.push(question.score[0]);
+              }
+            } else {
+              // Options questions
+              answersDimension[dimKey][qKey].general.scores.push(...question.score);
+            }
+            qCnt++;
           }
         }
       }
+      dimCnt++;
     }
 
     return {
-      scatter: answersForScatter,
-      evaluations: answersDimension
+      evaluations: answersDimension,
+      scatter: answersForScatter
     };
   }
 }
