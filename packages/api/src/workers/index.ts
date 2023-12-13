29c8d3cf8b8f7323472b8c78034eb24ef836c7a5
Octavio Torres <octa022@gmail.com>
2023-12-13 10:54:31 -0400

    style(api): indica fecha de última actualización.

--- packages/api/src/workers/index.ts
+++ packages/api/src/workers/index.ts
@@ -12,7 +12,7 @@ import ProcessReportOrganizational from './reports/organizational';
 const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
 export default () => {
-  setInterval(() => { console.log('Updated at 2023-12-08 19:00'); }, ms('5m'));
+  setInterval(() => { console.log('Updated at 2023-12-13 11:00'); }, ms('5m'));
   const quarter = '15m';
   const minutes = '5m';
   const seconds = '30s';
