892162c99ab356467d2f0df586e3eb975a844d14
Octavio Torres <octa022@gmail.com>
2023-12-21 10:58:08 -0400

    style(api): imprime banderas en generación de reporte por población.

--- packages/api/src/workers/reports/check-pending-reports.ts
+++ packages/api/src/workers/reports/check-pending-reports.ts
@@ -20,12 +20,14 @@ class ReportChecker {
       return;
     }
     await OperationThreadsService.findOneAndUpdateStatus(pendingOperationThread._id, 'in_action');
+    console.log('* * * * STEP 1-1 * * * *');
 
     const threadData = pendingOperationThread.data;
     threadData.step = 1;
     threadData.progress = 10;
 
     const evaluation: any = await EvaluationsService.findById(threadData._evaluation, 'populationLeaders');
+    console.log('* * * * STEP 1-2 * * * *');
     threadData.populationLeaders = evaluation ? evaluation.populationLeaders : [];
 
     switch (threadData.type) {
@@ -44,6 +46,7 @@ class ReportChecker {
         break;
       case 'by_demographic':
         const initByPopulation = await InitByPopulation(threadData.criteria);
+        console.log('* * * * STEP 1-3 * * * *');
         threadData.answersDimension = initByPopulation.answersDimension;
         threadData.answersForScatter = initByPopulation.answersForScatter;
         threadData.answersRateDetails = initByPopulation.rates;
@@ -58,6 +61,7 @@ class ReportChecker {
         break;
     }
 
+    console.log('* * * * STEP 1-4 * * * *');
     await OperationThreadsService.findOneAndUpdateStatusData(pendingOperationThread._id, 'in_progress', threadData);
 
     return pendingOperationThread._id;
