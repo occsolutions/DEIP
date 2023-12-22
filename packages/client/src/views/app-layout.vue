681495e9ba5d27de33153cd81984300e0095c95a
Octavio Torres <octa022@gmail.com>
2023-12-22 11:17:01 -0400

    style(client): indica fecha de última actualización.

--- packages/client/src/views/app-layout.vue
+++ packages/client/src/views/app-layout.vue
@@ -345,7 +345,7 @@ export default {
     const url = new URL(urlString)
     const updatedAt = url.searchParams.get('updatedAt')
     if (updatedAt) {
-      console.log('Updated at 2023-12-21 14:00')
+      console.log('Updated at 2023-12-22 11:00')
     }
   }
 }
