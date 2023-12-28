fe56cc6f0554a9a422234aeb28adc77b6464d674
Octavio Torres <octavio.torres@alphas.technology>
2023-12-28 11:35:37 -0400

    style(client): indica fecha de última actualización.

--- packages/client/src/views/app-layout.vue
+++ packages/client/src/views/app-layout.vue
@@ -345,7 +345,7 @@ export default {
     const url = new URL(urlString)
     const updatedAt = url.searchParams.get('updatedAt')
     if (updatedAt) {
-      console.log('Updated at 2023-12-27 12:00')
+      console.log('Updated at 2023-12-28 11:30')
     }
   }
 }
