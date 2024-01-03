00aa345b3eed12d8791cc2cbdd074e0df9a05d77
Octavio Torres <octavio.torres@alphas.technology>
2024-01-03 16:35:17 -0400

    style(api): indica fecha de última actualización.

--- packages/api/src/workers/index.ts
+++ packages/api/src/workers/index.ts
@@ -12,7 +12,7 @@ import ProcessReportByPopulation from './reports/by-population';
 const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
 export default () => {
-  setInterval(() => { console.log('Updated at 2023-12-22 11:00'); }, ms('5m'));
+  setInterval(() => { console.log('Updated at 2024-01-03 16:30'); }, ms('5m'));
   const quarter = '15m';
   const minutes = '5m';
   const seconds = '30s';
